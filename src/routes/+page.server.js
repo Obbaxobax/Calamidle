270025b4c638b7d29dcfd9f2b0f24501ba3f1978
Obbaxobax <nickyedstrom@gmail.com>
2024-05-03 20:38:49 -0600

    fix env

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,10 @@
-import { BASENUMBER, M, A } from "$env/static/private"
+import { BASENUMBER, MNUMBER, ANUMBER } from "$env/static/private"
 import weapons from "$lib/data/weapons.json"
 
 export async function load({fetch}){
     const c = parseInt(BASENUMBER) / 1000
     const day = Math.round(Date.now() / 86400000)
-    let num = ((day * parseFloat(A) + c) % parseFloat(M))
+    let num = ((day * parseFloat(ANUMBER) + c) % parseFloat(MNUMBER))
     num = Math.round(num * 100000)
     num = num % Object.keys(weapons).length == 0 ? num : num % Object.keys(weapons).length
     
